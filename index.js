9bb33dc403580459adf0b32f76fe9c9bb72077ad
Nikapwente <hrsmajor@gmail.com>
2023-10-16 23:06:51 +0300

    Deprecation Warning

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+
+function submitData(userName,userEmail) {
+return fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+        "Content-Type":"application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        name: `${userName}`,
+        email: `${userEmail}`,
+    }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        let header = document.getElementById("header");
+        document = data.id; 
+        header.textContent = data.id; 
+        
+    })
+    .catch(error => {
+        alert("Bad things! Ragnarők!");
+        let header = document.getElementById("header");
+        document = error.message;
+        header.textContent = error.message;
+        
+    })
+}
